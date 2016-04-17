84c1fe3d6d8e95518d5ed4a291a72874af70df9f
Nitesh Pandey <n@nitesh.us>
2016-04-17 10:13:31 -0700

    Add new routes for Account pages

--- src/containers/MyAccountPage.js
+++ src/containers/MyAccountPage.js
@@ -1,15 +1,13 @@
 import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { requestBeHost } from '../actions/userActions'
+import CreateMenuForm from '../components/CreateMenuForm'
 
 class MyAccountPage extends React.Component {
   render () {
     const {user, requestBeHost} = this.props;
       return user?(
-        <div>
-          <h1> I am of User type {user.type || 'diner'}</h1>
-          <button onClick={() => requestBeHost(user._id)} >Be Host!</button>
-        </div>
+        <div>{this.props.children}</div>
       ):<h1>Loading</h1>;
 
 
